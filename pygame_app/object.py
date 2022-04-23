de3be9b1fbd3633fe4d06cf282391c161ef74128
alex <alex@localohost>
2022-04-23 09:57:38 -0400

    added update() to the Object protocol class

--- pygame_app/object.py
+++ pygame_app/object.py
@@ -5,3 +5,7 @@ class Object(Protocol):
     def draw(self, surface) -> None:
         """Defines a function that will be called to draw the object"""
         pass
+
+    def update(self) -> None:
+        """Defines a function that will be called to update object state each frame"""
+        pass
